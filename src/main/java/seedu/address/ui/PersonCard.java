798407e2eda1aef7ad4fc2e00c615ebdad334b2c
Liao Yantong <e1484072@u.nus.edu>
2026-04-04 19:17:20 +0800

    fix style issue

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -85,7 +85,6 @@ public class PersonCard extends UiPart<Region> {
                 resLabel.getStyleClass().add("cell_small_label");
                 resLabel.setStyle("-fx-text-fill: #FFD700;"); // Gold
                 loansContainer.getChildren().add(resLabel);
-              
                 Label status = new Label("RESERVED");
                 // Orange for reserved.
                 configureStatusStyle(status, "#ffa000");
